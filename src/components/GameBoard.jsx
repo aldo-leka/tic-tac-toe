c2eb535b59dc2ef50f2a84305ed107ed37b0f081
Aldo <aldo.leka@live.com>
2025-03-02 10:05:54 +0100

    Lecture 88 - game board buttons can't be clicked twice.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -4,11 +4,11 @@ const initialGameBoard = [
     [null, null, null]
 ];
 
-export default function GameBoard({ onSelectSquare, turns }) {
+export default function GameBoard({onSelectSquare, turns}) {
     let gameBoard = initialGameBoard;
     for (const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
+        const {square, player} = turn;
+        const {row, col} = square;
 
         gameBoard[row][col] = player;
     }
@@ -26,16 +26,17 @@ export default function GameBoard({ onSelectSquare, turns }) {
     // }
 
     return (
-      <ol id="game-board">
-          {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
-              <ol>
-                  {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                      <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
-                          {playerSymbol}
-                      </button>
-                  </li>)}
-              </ol>
-          </li>)}
-      </ol>
+        <ol id="game-board">
+            {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
+                <ol>
+                    {row.map((playerSymbol, colIndex) =>
+                        <li key={colIndex}>
+                            <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>
+                                {playerSymbol}
+                            </button>
+                        </li>)}
+                </ol>
+            </li>)}
+        </ol>
     );
 }
