e93b2f2160a4d7da0de91d2b0adaf59c021b2cfc
Aldo <aldo.leka@live.com>
2025-03-02 10:46:43 +0100

    Lecture 92 - Add Game over state

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GameBoard from "./components/GameBoard.jsx";
 import {useState} from "react";
 import Log from "./components/Log.jsx";
 import {WINNING_COMBINATIONS} from "./winning-combinations.js";
+import GameOver from "./components/GameOver.jsx";
 
 const initialGameBoard = [
     [null, null, null],
@@ -48,6 +49,8 @@ function App() {
         }
     }
 
+    const hasDraw = gameTurns.length === 9 && !winner;
+
     function handleSelectSquare(rowIndex, colIndex) {
         // setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
         setGameTurns(prevTurns => {
@@ -68,7 +71,7 @@ function App() {
                 <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
                 <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
             </ol>
-            {winner && <p>You won, {winner}</p>}
+            {(winner || hasDraw ) && <GameOver winner={winner} />}
             <GameBoard
                 onSelectSquare={handleSelectSquare}
                 board={gameBoard}
